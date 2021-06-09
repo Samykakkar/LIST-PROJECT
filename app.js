aa85b5cbde85c5e6f589099ad8db167f8df7ec6e
Sameer <sameer@Sameers-MacBook-Pro.local>
2021-06-09 14:13:32 +0530

    LIST PROJECT

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ while (input !== 'quit' && input !== 'q') {
         for(let i=0;i<todos.length;i++){
             console.log(`${i}: ${todos[i]}`)
         }
+    }else if(input==='new'){
+        const newtodo=prompt("Ok, What is the new todo?")
+        todos.push(newtodo)
+        console.log(`$(newtodo) is added to the list`)
     }
+
     input = prompt("what do you like to do?")
 }
 console.log("OK QUIT THE APP")
